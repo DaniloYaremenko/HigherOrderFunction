372895a63f46a6138a49f4d7e415150c1630eafd
DanilYaremenko <danilyaremenko@gmail.com>
2022-01-03 20:19:22 +0200

    Update

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  const keysArray = Object.keys(obj);
+  for (const key of keysArray) {
+    const value = obj[key];
+    callback(key, value, obj);
+  }
+};
 
 module.exports = { iterate };
